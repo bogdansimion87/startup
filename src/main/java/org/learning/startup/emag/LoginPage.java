9d4d2b80957a8bf87275e981fed3b8e6d242f5be
bogdansimion87 <bogdan_simion87@yahoo.com>
2016-05-09 18:05:43 +0300

    Revert "removed import statements"
    This reverts commit f5a7e23c809e2713912f3a1951266ff60b952a6f.

--- src/main/java/org/learning/startup/emag/LoginPage.java
+++ src/main/java/org/learning/startup/emag/LoginPage.java
@@ -1,30 +1,17 @@
 package org.learning.startup.emag;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginPage {
 	private WebDriver driver;
 	
 	public LoginPage(WebDriver driver) {
 		this.setDriver(driver);
-		
-		// Create a new instance of the Firefox driver
-	    driver = new FirefoxDriver();
-	    
-	    //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception	 
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    
-	    //Launch the  Website
-	    driver.get("https://www.emag.ro/user/login");
 	
 	}
 
-	
 	public WebDriver getDriver() {
 		return driver;
 	}
