f5a7e23c809e2713912f3a1951266ff60b952a6f
bogdansimion87 <bogdan_simion87@yahoo.com>
2016-05-09 18:04:31 +0300

    removed import statements

--- src/test/java/org/learning/startup/SimpleTest.java
+++ src/test/java/org/learning/startup/SimpleTest.java
@@ -1,16 +1,8 @@
 package org.learning.startup;
 
-
-import java.util.concurrent.TimeUnit;
-
 import org.learning.startup.emag.LoginPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
@@ -36,20 +28,6 @@ public class SimpleTest {
 	  Assert.assertEquals(loginPage.getLogginErrorLabel(), "Ai introdus gresit parola sau adresa de email. Te rog completeaza din nou.");
   }
   
-  
-  
-  @BeforeMethod
-  public void beforeMethod() {
-	  
-	// Create a new instance of the Firefox driver
-    driver = new FirefoxDriver();
-	
-	//Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception	 
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			
-	//Launch the  Website
-    driver.get("https://www.emag.ro/user/login");
-  }
 
   
   
